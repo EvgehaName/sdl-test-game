dfcb60ceccb8fa7c8f3d893769154adb1776608e
EvgehaName <dubasov.eugeni@yandex.ru>
2026-01-26 16:36:58 +0300

    Реализация тестовой окончальной системы FPS.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <chrono>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
@@ -17,7 +19,7 @@ int main(int argc, char* argv[])
     const char* currentDrivers = SDL_GetCurrentVideoDriver();
     std::cout << "Selected driver: " << currentDrivers << std::endl;
     window = SDL_CreateWindow(
-        "SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_OPENGL);
+        "SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, 0);
     SDL_SetWindowResizable(window, SDL_TRUE);
     if (window == nullptr) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to create SDL window: %s", SDL_GetError());
@@ -31,7 +33,7 @@ int main(int argc, char* argv[])
     dRectPlayer.x = 0;
     dRectPlayer.y = 0;
 
-    renderer = SDL_CreateRenderer(window, -1, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 
     if(renderer == nullptr)
     {
@@ -43,16 +45,45 @@ int main(int argc, char* argv[])
     SDL_Surface *surface = IMG_Load("player.png");
     playerTexture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
+
+
     
-    int FPS = 60;
-    int frameDelay = 1000 / FPS; 
-    Uint32 frameStart;
-    int frameTime;
+    //Uint32 lastTime = SDL_GetTicks();
+    //Uint32 fpsTimer = SDL_GetTicks();
+    std::chrono::time_point lastTime = std::chrono::system_clock::now();
+    //Uint32 fpsTimer = SDL_GetTicks();
+    std::chrono::time_point fpsTimer = std::chrono::system_clock::now();
+    float limitFPS = 60.0f;
+    float targetLimit = limitFPS / 1000;
+    int frameCount = 0;
+    int fps = 0;
+    float deltaTime = 0;
+    float speedPlayer = 500.0f;
+    float posX = 0.0f;
+    float posY = 0.0f;
+
     double angle = 0;
     while (!done) {
-        frameStart = SDL_GetTicks();
+        //Uint32 currentTime = SDL_GetTicks();
+        std::chrono::time_point currentTime = std::chrono::system_clock::now();
+        std::chrono::duration<float> tim = (currentTime - lastTime);
+        deltaTime = tim.count();
+        lastTime = currentTime;
+        std::cout << "DELTA: " << deltaTime << std::endl;
+
+
+        frameCount++;
+        std::chrono::duration<float> dd = currentTime - fpsTimer;
+        if (dd.count() >= 1.0f) {
+            fps = frameCount;
+            std::string titleFPS = "FPS: " + std::to_string(fps);
+            SDL_SetWindowTitle(window, titleFPS.c_str());
+            frameCount = 0;
+            fpsTimer = currentTime;
+        }
+        
         SDL_Event event;
-        SDL_PumpEvents();
+
         SDL_PollEvent(&event);
         switch (event.type)
         {
@@ -60,8 +91,8 @@ int main(int argc, char* argv[])
             done = true;
             break;
         case SDL_MOUSEMOTION:
-            std::cout << "mouse X: " << event.motion.x << "mouse Y: " << event.motion.y << std::endl;
-            std::cout << "angle: " << angle << std::endl;
+            //std::cout << "mouse X: " << event.motion.x << "mouse Y: " << event.motion.y << std::endl;
+            //std::cout << "angle: " << angle << std::endl;
             double dy = event.motion.y - dRectPlayer.y;
             double dx = event.motion.x - dRectPlayer.x;
             angle = atan2(dy, dx) * (180.0 / M_PI);
@@ -69,18 +100,32 @@ int main(int argc, char* argv[])
         }
         dRectPlayer.h = 64;
         dRectPlayer.w = 64;
-        dRectPlayer.x = 500 / 2;
-        dRectPlayer.y = 500 / 2;
+       
+        const Uint8* state = SDL_GetKeyboardState(NULL);
+        if (state[SDL_SCANCODE_W])
+        {
+            posY -= speedPlayer * deltaTime;
+        }
+        if (state[SDL_SCANCODE_S])
+        {
+            posY += speedPlayer * deltaTime;
+        }
+        if (state[SDL_SCANCODE_A])
+        {
+             posX -= speedPlayer * deltaTime;
+        }
+        if (state[SDL_SCANCODE_D])
+        {
+            posX += speedPlayer * deltaTime;
+        }
+        dRectPlayer.y = (int)posY;
+        dRectPlayer.x = (int)posX;
 
         SDL_RenderClear(renderer);
         SDL_RenderCopyEx(renderer, playerTexture, nullptr, &dRectPlayer, angle, nullptr, SDL_FLIP_NONE);
         SDL_RenderPresent(renderer);
-        std::cout << "X: " << dRectPlayer.x << " "  << "Y: " << dRectPlayer.y <<std::endl;
-        frameTime = SDL_GetTicks() - frameStart;
-        if(frameDelay > frameTime)
-        {
-            SDL_Delay(frameDelay - frameTime);
-        }
+        //std::cout << "X: " << dRectPlayer.x << " "  << "Y: " << dRectPlayer.y <<std::endl;
+        SDL_Delay(3);
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
